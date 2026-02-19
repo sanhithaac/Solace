0bc95d4099d73265e313202c2862139eb51659ae
Allampati Chenchu Sanhitha Reddy <sanhithaac@gmail.com>
2026-02-20 00:50:24 +0530

    Add dynamic export to mood route

--- app/api/mood/route.ts
+++ app/api/mood/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import connectToDatabase from "@/lib/mongodb";
 import MoodLog from "@/models/MoodLog";
 import User from "@/models/User";
+export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
     try {
